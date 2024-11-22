09467a5bf61b431a7ac85cd09d258923a46a16fb
mReidMoesker <opqetch@gmail.com>
2024-11-22 13:22:09 -0600

    scripts

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const click = [];
+
+let clickCount = 0;
+
+
+const button = document.querySelector('.click-button');
+const display = document.querySelector('.click-count');
+
+
+
+button.addEventListener('click', () => {clickCount++; display.innerText = clickCount;});
