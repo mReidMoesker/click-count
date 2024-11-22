2fd75acf1847e186398afbb1aec9fc0916bb7514
mReidMoesker <opqetch@gmail.com>
2024-11-22 13:43:12 -0600

    finalized script

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,7 @@ const click = [];
 
 let clickCount = 0;
 
-
 const button = document.querySelector('.click-button');
 const display = document.querySelector('.click-count');
 
-
-
-button.addEventListener('click', () => {clickCount++; display.innerText = clickCount;});
+button.addEventListener('click', () => {clickCount++; display.innerText = `You clicked ${clickCount} times`;});
